6580e0f8549f7ea8a52c3fc329dcde9ce4c502ba
aakbarkhan <aakuukhan@gmail.com>
2021-10-19 00:02:11 +0530

    Fix linters

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-const presenterCard = document.querySelector('#presenter-card');
+const presenterCard = document.querySelector('#presenters-card');
 
 const data = [
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    },
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    },
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    },
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    },
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    },
-    {
-        name:'Elon Musk',
-        position:'CEO&Founder',
-        description:'',
-        image:'',
-        imageAlt:'Elon-musk'
-    }
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
+  {
+    name: 'Elon Musk',
+    position: 'CEO&Founder',
+    description: '',
+    image: '',
+    imageAlt: 'Elon-musk',
+  },
 ];
