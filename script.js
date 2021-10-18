ee248d8d21dd9ca2aca43ff7c019bd98ad72fa19
aakbarkhan <aakuukhan@gmail.com>
2021-10-18 23:50:06 +0530

    Add js data

---
+++ script.js
@@ -0,0 +1,46 @@
+const presenterCard = document.querySelector('#presenter-card');
+
+const data = [
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    },
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    },
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    },
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    },
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    },
+    {
+        name:'Elon Musk',
+        position:'CEO&Founder',
+        description:'',
+        image:'',
+        imageAlt:'Elon-musk'
+    }
+];
